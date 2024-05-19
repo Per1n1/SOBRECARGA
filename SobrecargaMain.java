0491316a6cc12f8e16571938075e770632a294f8
Per1n1 <151886360+Per1n1@users.noreply.github.com>
2024-05-19 20:28:41 -0300

    SobrecargaMain.java

---
+++ SobrecargaMain.java
@@ -0,0 +1,18 @@
+package sobrecarga;
+
+public class Main {
+    public static void main(String[] args) {
+            Calculadora calc = new Calculadora();
+
+
+            int resultado1 = calc.somar(5, 10);
+            int resultado2 = calc.somar(5, 10, 15);
+            double resultado3 = calc.somar(3.5, 2.7);
+
+            System.out.println("Resultado 1: " + resultado1);
+            System.out.println("Resultado 2: " + resultado2);
+            System.out.println("Resultado 3: " + resultado3);
+        }
+    }
+
+
